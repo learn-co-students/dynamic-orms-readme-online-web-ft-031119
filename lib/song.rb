ab2c1ea50e55ff78ffa6d184fa5b44ba585bf096
github username <github email address>
2019-04-18 22:47:08 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,6 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class Song
-
-
   def self.table_name
     self.to_s.downcase.pluralize
   end
@@ -29,10 +27,12 @@ class Song
     options.each do |property, value|
       self.send("#{property}=", value)
     end
+    
   end
 
   def save
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
@@ -43,6 +43,7 @@ class Song
 
   def values_for_insert
     values = []
+
     self.class.column_names.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
@@ -55,10 +56,8 @@ class Song
 
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+
     DB[:conn].execute(sql)
   end
 
 end
-
-
-
